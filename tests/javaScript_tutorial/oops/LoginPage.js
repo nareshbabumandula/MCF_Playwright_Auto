6402f67716fbe6a5e2a4aee0d8cc5ef04f370e1d
anikethchand <anikethchandgochikonda@gmail.com>
2026-01-24 22:06:47 +0530

    deleted .js files

--- tests/javaScript_tutorial/oops/LoginPage.js
+++
@@ -1,8 +0,0 @@
-import { BasePage } from "./BasePage";
-export class LoginPage extends BasePage {
-    async login(username, password) {
-        await this.type('input[id="user"]', username);
-        await this.type('input[id="pass"]', password);
-        await this.click('input[name="btnSubmit"]');
-    }
-}
