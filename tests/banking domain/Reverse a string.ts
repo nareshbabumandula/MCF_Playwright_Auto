4b514628195f9f25e1bfb084d7dbf8307d3e4672
anikethchand <anikethchandgochikonda@gmail.com>
2026-02-24 10:54:07 +0530

    deleted files

--- tests/banking domain/Reverse a string.ts
+++
@@ -1,8 +0,0 @@
-function reverseString(str: string): string {
-    return str.split('').reverse().join('');
-}
-
-// Example usage
-const input = "hello";
-const reversedResult = reverseString(input);
-console.log(reversedResult); // "olleh"
